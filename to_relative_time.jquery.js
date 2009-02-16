a3caae78264d2c38579aef32b3d9593b254babfe
James Herdman <james.herdman@gmail.com>
2009-02-16 12:56:02 -0500

    A jQuery wrapper for relative time helpers

---
+++ to_relative_time.jquery.js
@@ -0,0 +1,22 @@
+$(function() {
+    /*
+     * A handy jQuery wrapper for converting tags with JavaScript parse()-able
+     * time-stamps into relative time strings.
+     *
+     * Usage:
+     *   Suppose numerous Date.parse()-able time-stamps are available in the 
+     *   inner-HTML of some <span class="rel"> elements...
+     *
+     *   $("span.rel").toRelativeTime()
+     *
+     * Examples: '5 years ago', '45 minutes ago'
+     *
+     * Requires date.extensions.js to be loaded first.
+     */
+    $fn.toRelativeTime = function {
+      this.each(function() {
+        var elem = $(this);
+        elem.text(Date.fromString(e.html()).toRelativeTime());
+      });
+    }
+  });
