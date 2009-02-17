19bf615531fd4b40a862068efcb0441a5ad52e56
James Herdman <james.herdman@gmail.com>
2009-02-16 23:03:32 -0500

    Add a Mootools wrapper and test

---
+++ to_relative_time.mootools.js
@@ -0,0 +1,7 @@
+window.addEvent("domready", function() {
+  Date.allToRelativeTime = function(elems) {
+    $each(elems, function(elem) {
+      elem.set("text", Date.fromString(elem.get("text")).toRelativeTime());
+    });
+  }
+});
