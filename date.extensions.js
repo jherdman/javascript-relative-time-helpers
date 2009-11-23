19db3add17607171f19fb5795ffa771927fc5314
James Herdman <james.herdman@gmail.com>
2009-11-23 18:23:34 -0500

    Tight stuff. Just didn't like the whitespace

--- date.extensions.js
+++ date.extensions.js
@@ -1,35 +1,36 @@
 /**
- * Returns a description of this past date in relative terms. 
+ * Returns a description of this past date in relative terms.
  * Takes an optional parameter (default: 0) setting the threshold in ms which
  * is considered "Just now".
- * 
+ *
  * Examples, where new Date().toString() == "Mon Nov 23 2009 17:36:51 GMT-0500 (EST)":
- * 
+ *
  * new Date().toRelativeTime()
  * --> 'Just now'
- * 
+ *
  * new Date("Nov 21, 2009").toRelativeTime()
  * --> '2 days ago'
- * 
+ *
  * // One second ago
  * new Date("Nov 23 2009 17:36:50 GMT-0500 (EST)").toRelativeTime()
  * --> '1 second ago'
- * 
+ *
  * // One second ago, now setting a now_threshold to 5 seconds
  * new Date("Nov 23 2009 17:36:50 GMT-0500 (EST)").toRelativeTime(5000)
  * --> 'Just now'
- * 
+ *
  */
-Date.prototype.toRelativeTime = function( now_threshold ) {
+Date.prototype.toRelativeTime = function(now_threshold) {
   var delta = new Date() - this;
-  
+
   now_threshold = parseInt(now_threshold, 10);
-  if ( isNaN(now_threshold) )
+
+  if (isNaN(now_threshold))
     now_threshold = 0;
-  
+
   if (delta <= now_threshold)
     return 'Just now';
-  
+
   var units = null;
   var conversions = {
     millisecond: 1, // ms    -> ms
@@ -40,7 +41,7 @@ Date.prototype.toRelativeTime = function( now_threshold ) {
     month:  30,     // day   -> month (roughly)
     year:   12      // month -> year
   };
-  
+
   for (var key in conversions) {
     if (delta < conversions[key]) {
       break;
@@ -49,7 +50,7 @@ Date.prototype.toRelativeTime = function( now_threshold ) {
       delta = delta / conversions[key];
     }
   }
-  
+
   // pluralize a unit when the difference is greater than 1.
   delta = Math.floor(delta);
   if (delta !== 1) { units += "s"; }
@@ -57,7 +58,7 @@ Date.prototype.toRelativeTime = function( now_threshold ) {
 };
 
 /*
- * Wraps up a common pattern used with this plugin whereby you take a String 
+ * Wraps up a common pattern used with this plugin whereby you take a String
  * representation of a Date, and want back a date object.
  */
 Date.fromString = function(str) {
